1540c8607949e870a500be0785abe755d911a0b1
Nieves Yashuang López <nieveslpez.p@gmail.com>
2025-09-23 15:33:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*  Base code by Alun Evans 2016 LaSalle (aevanss@salleurl.edu) modified by: Conrado Ruiz, Ferran Ruiz 2024*/
 
-// student name: 
+// student name: Nieves Yashuang Lopez, Lucia Perez
 
 //include some standard libraries
 #include <stdio.h>
@@ -39,64 +39,169 @@ void load()
 
 	//the positions of the corner of the square, each a 3-component vector, x, y, z
 	const GLfloat vertices[] = 
+		
+		{	
 		//Right ear
-		{	0.3f,  0.55f, 0.0f,		// vertex 0
-			0.6f,  0.8f, 0.0f,		// vertex 1
-		    0.8f,  0.5f, 0.0f,		// vertex 2
-		    0.6f,  0.05f, 0.0f,		// vertex 3
+			0.4f,  0.65f, 0.0f,		// vertex 0
+			0.7f,  0.8f, 0.0f,		// vertex 1
+		    0.9f,  0.5f, 0.0f,		// vertex 2
+		    0.7f,  0.15f, 0.0f,		// vertex 3
+
 		//Left ear
-			-0.3f,  0.55f, 0.0f,	// vertex 4
-			-0.6f,  0.8f, 0.0f,		// vertex 5
-			-0.8f,  0.5f, 0.0f,		// vertex 6
-			-0.6f,  0.05f, 0.0f,	// vertex 7
+			-0.4f,  0.65f, 0.0f,	// vertex 4
+			-0.7f,  0.8f, 0.0f,		// vertex 5
+			-0.9f,  0.5f, 0.0f,		// vertex 6
+			-0.7f,  0.15f, 0.0f,	// vertex 7
+
 		//Right Upper Head
 			 0.0f,  0.7f, 0.0f,		// vertex 8
-			 0.3f,  0.55f, 0.0f,	// vertex 9
-			 0.1f,  0.2f, 0.0f,		// vertex 10
+			 0.4f,  0.65f, 0.0f,	// vertex 9
+			 0.2f,  0.2f, 0.0f,		// vertex 10
 			 0.0f,  0.2f, 0.0f,		// vertex 11
+
 		//Left Upper Head
-			-0.0f,  0.7f, 0.0f,		// vertex 8
-			-0.3f,  0.55f, 0.0f,	// vertex 9
-			-0.1f,  0.2f, 0.0f,		// vertex 10
-			-0.0f,  0.2f, 0.0f };	// vertex 11
+			 0.0f,  0.7f, 0.0f,		// vertex 12
+			-0.4f,  0.65f, 0.0f,	// vertex 13
+			-0.2f,  0.2f, 0.0f,		// vertex 14
+			 0.0f,  0.2f, 0.0f, 	// vertex 15
+
+		//Right lateral face
+			 0.2f, 0.2f, 0.0f,		// vertex 16
+			 0.4f,  0.65f, 0.0f,	// vertex 17
+			 0.8f,  -0.02f, 0.0f,	// vertex 18
+			 0.65f,  -0.5f, 0.0f,	// vertex 19
+
+		//Left lateral face
+			  -0.2f, 0.2f, 0.0f,	// vertex 20
+			  -0.4f,  0.65f, 0.0f,	// vertex 21
+			  -0.8f,  -0.02f, 0.0f,	// vertex 22
+			  -0.65f,  -0.5f, 0.0f,	// vertex 23
+
+		//Right middle face
+			  0.0f,  0.2f, 0.0f,	// vertex 24
+			  0.2f,  0.2f, 0.0f,	// vertex 25
+			  0.22f,  -0.25f, 0.0f,	// vertex 26
+			  0.0f, -0.25f, 0.0f,	// vertex 27
+
+		 //Left middle face
+			  0.0f,  0.2f, 0.0f,	// vertex 28
+			  -0.2f,  0.2f, 0.0f,	// vertex 29
+			  -0.22f,  -0.25f, 0.0f,// vertex 30
+			  0.0f, -0.25f, 0.0f,	// vertex 31
+
+		//Right eye
+			  0.22f,  -0.25f, 0.0f,	// vertex 32
+			  0.2f,  0.2f, 0.0f,	// vertex 33
+			  0.55f, -0.35f, 0.0f,	// vertex 34
+			  0.32f,  -0.4f, 0.0f,	// vertex 35
+
+		//Left eye
+			  -0.22f,  -0.25f, 0.0f,// vertex 36
+			  -0.2f,  0.2f, 0.0f,	// vertex 37
+			  -0.55f, -0.35f, 0.0f,	// vertex 38
+			  -0.32f,  -0.4f, 0.0f,	// vertex 39
+
+		};
 
 	//we assign a colour to each corner (each colour is RGB)
 	const GLfloat colors[] = 
-		//Right ear
-		{	0.0f, 0.0f, 0.0f,		//Vertex 0.black
-			0.0f, 0.0f, 0.0f,		//Vertex 1.black
-			0.0f, 0.0f, 0.0f,		//Vertex 2.black
-			0.0f, 0.0f, 0.0f,		//Vertex 3.black
-		//Left ear
-			0.0f, 0.0f, 0.0f,		//Vertex 4.black
-			0.0f, 0.0f, 0.0f,		//Vertex 5.black
-			0.0f, 0.0f, 0.0f,		//Vertex 6.black
-			0.0f, 0.0f, 0.0f,		//Vertex 7.black
+
+		{
+		//Right ear 
+		0.4f, 0.25f, 0.1f,      //Vertex 0 
+		0.35f, 0.2f, 0.08f,     //Vertex 1 
+		0.3f, 0.15f, 0.06f,     //Vertex 2 
+		0.45f, 0.28f, 0.12f,    //Vertex 3 
+
+		//Left ear 
+		0.4f, 0.25f, 0.1f,      //Vertex 4 
+		0.35f, 0.2f, 0.08f,     //Vertex 5 
+		0.3f, 0.15f, 0.06f,     //Vertex 6 
+		0.45f, 0.28f, 0.12f,    //Vertex 7 
+
 		//Right Upper Head
-			1.0f, 1.0f, 1.0f,		//Vertex 6.white
-			1.0f, 1.0f, 1.0f,		//Vertex 7.white
-			1.0f, 1.0f, 1.0f,		//Vertex 8.white
-			1.0f, 1.0f, 1.0f,		//Vertex 9.white
-		//Left Upper Head
-			1.0f, 1.0f, 1.0f,		//Vertex 10.white
-			1.0f, 1.0f, 1.0f,		//Vertex 11.white
-			1.0f, 1.0f, 1.0f,		//Vertex 12.white
-			1.0f, 1.0f, 1.0f };		//Vertex 13.white
+		0.6f, 0.4f, 0.2f,       //Vertex 8
+		0.65f, 0.45f, 0.25f,    //Vertex 9 
+		0.55f, 0.35f, 0.18f,    //Vertex 10 
+		0.58f, 0.38f, 0.19f,    //Vertex 11 
+
+		//Left Upper Head 
+		0.6f, 0.4f, 0.2f,       //Vertex 12 
+		0.65f, 0.45f, 0.25f,    //Vertex 13 
+		0.55f, 0.35f, 0.18f,    //Vertex 14 
+		0.58f, 0.38f, 0.19f,    //Vertex 15 
+
+		//Right lateral face 
+		0.52f, 0.32f, 0.15f,    //Vertex 16 
+		0.58f, 0.38f, 0.19f,    //Vertex 17
+		0.48f, 0.28f, 0.12f,    //Vertex 18 
+		0.45f, 0.25f, 0.1f,     //Vertex 19 
+
+		//Left lateral face 
+		0.52f, 0.32f, 0.15f,    //Vertex 20
+		0.58f, 0.38f, 0.19f,    //Vertex 21 
+		0.48f, 0.28f, 0.12f,    //Vertex 22 
+		0.45f, 0.25f, 0.1f,     //Vertex 23 
+
+		//Right middle face
+		0.62f, 0.42f, 0.22f,    //Vertex 24 
+		0.6f, 0.4f, 0.2f,       //Vertex 25 
+		0.58f, 0.38f, 0.19f,    //Vertex 26
+		0.6f, 0.4f, 0.2f,       //Vertex 27 
+
+		//Left middle face 
+		0.62f, 0.42f, 0.22f,    //Vertex 28 
+		0.6f, 0.4f, 0.2f,       //Vertex 29 
+		0.58f, 0.38f, 0.19f,    //Vertex 30 
+		0.6f, 0.4f, 0.2f,        //Vertex 31
+
+		//Right eye
+		0.0f, 0.0f, 0.0f,		//Vertex 28 
+		0.0f, 0.0f, 0.0f,       //Vertex 29 
+		0.0f, 0.0f, 0.0f,    	//Vertex 30
+		0.0f, 0.0f, 0.0f,        //Vertex 31 
+
+		//Leftt eye
+		0.0f, 0.0f, 0.0f,		//Vertex 28 
+		0.0f, 0.0f, 0.0f,       //Vertex 29 
+		0.0f, 0.0f, 0.0f,    	//Vertex 30 
+		0.0f, 0.0f, 0.0f        //Vertex 31 
+		};
 
 	// The index buffer references the vertices we paint, in order
 	//here we have two triangles
 	const GLuint indices[] = 
 		{	0, 1, 2,				// triangle 0
-			0, 2, 3,
+			0, 2, 3,				// triangle 1
 
-			4, 5, 6,
-			4, 6, 7,
+			4, 5, 6,				// triangle 2
+			4, 6, 7,				// triangle 3
 
-			8, 9, 10,
-			8, 10, 11,
+			8, 9, 10,				// triangle 4
+			8, 10, 11,				// triangle 5
 
-			12, 13, 14,
-			12, 14, 15};				
+			12, 13, 14,				// triangle 6
+			12, 14, 15,				// triangle 7
+	
+			16, 17, 18,				// triangle 8
+			16, 18, 19,				// triangle 9
+	
+			20, 21, 22,				// triangle 10
+			20, 22, 23,				// triangle 11
+	
+			24, 25, 26,				// triangle 12
+			24, 26, 27,				// triangle 13
+	
+			28, 29, 30,				// triangle 14
+			28, 30, 31,				// triangle 15
+			
+			32, 33, 34,				// triangle 16
+			32, 34, 35,				// triangle 17
+
+			36, 37, 38,				// triangle 18
+			36, 38, 39				// triangle 19
+	
+		};				
 
 
  	//**********************
